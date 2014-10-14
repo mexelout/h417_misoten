deedbff573590d0c8f05e974b56cc849457f5f54
杉山元和 <mexelout@gmail.com>
2014-10-14 12:31:54 +0900

    ジャンプの挙動変更

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,12 @@ public class Player : MonoBehaviour {
 	public float speed;
 	public float speedDefault = 30;
 	public int lane;
+
+	// 飛んでる最中かどうかのフラグなのです！
+	public bool isFly;
+	private bool isOffBuilding;
+
 	private GameManager gm;
-    protected static SoundManager SoundDevice;  //サウンド再生用デバイス
 	private Animator anm;
 	private int anmSpeedHash;
 	private int anmJumpHash;
@@ -28,6 +32,7 @@ public class Player : MonoBehaviour {
 		jumpPower = jumpPowerDefault;
 		frameCount = 0;
 		isPlay = true;
+		isFly = false;
 	}
 
 	void Update() {
@@ -35,7 +40,7 @@ public class Player : MonoBehaviour {
 
 		float horizontal = Input.GetAxis("Horizontal");
 
-		if(gm.GetStartCount() > 0)
+		if(gm != null && gm.GetStartCount() > 0)
 			vertical *= 0;
 
 		transform.Translate(0, 0, vertical);
@@ -59,7 +64,7 @@ public class Player : MonoBehaviour {
 			}
 		}
 
-		if(gm.GetStartCount() > 0 && isPlay) {
+		if(gm != null && gm.GetStartCount() > 0 && isPlay) {
 			frameCount++;
 		}
 	}
@@ -68,10 +73,20 @@ public class Player : MonoBehaviour {
 		Vector3 dir = collision.gameObject.transform.up * -1;
 		transform.LookAt(dir + transform.position);
 		anm.SetBool(anmJumpHash, false);
+		isFly = false;
+		isOffBuilding = false;
+		CancelInvoke("OffTheGroundLittle");
 	}
 
 	private void OnCollisionStay(Collision collision) {
 		anm.SetBool(anmJumpHash, false);
+	}
+
+	private void OnCollisionExit(Collision collision) {
+		if(collision.gameObject.CompareTag("Building")) {
+			isOffBuilding = true;
+			Invoke("OffTheGroundLittle", 0.5f);
+		}
 	}
 
 	private void OnTriggerEnter(Collider collider) {
@@ -100,7 +115,6 @@ public class Player : MonoBehaviour {
 			}
 			try {
 				SceneManager sm = FindObjectOfType<SceneManager>();
-				
 				sm.NextScene();
 			} catch {
 				print("not found scene manager...");
@@ -117,10 +131,18 @@ public class Player : MonoBehaviour {
 		if(collider.gameObject.CompareTag("Bonus")) {
 			jumpPower = jumpPowerDefault;
 		}
-		print(collider.gameObject.tag);
+
+		print(collider.tag);
 	}
 
 	private void undoSpeed() {
 		speed = speedDefault;
+	}
+
+	private void OffTheGroundLittle() {
+		if(isOffBuilding) {
+			anm.SetBool(anmJumpHash, true);
+			isFly = true;
+		}
 	}
 }
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Player player = GameManager.FindObjectOfType<Player>();
+		Vector3 camera_pos = gameObject.transform.position;
+		Vector3 back = player.transform.forward * -1;
+		if(player.isFly) {
+			back.y = 0;
+			back = back.normalized * 4.5f;
+			back.y += 2;
+		} else {
+			back *= 2.5f;
+			back += player.transform.up * 2;
+		}
+		Vector3 purpose_pos = player.gameObject.transform.position + back;
+		Vector3 diff_vec = purpose_pos - camera_pos;
+		gameObject.transform.position = camera_pos + diff_vec / 5;
+		gameObject.transform.LookAt(player.transform.position + player.transform.up * 1.25f);
+	}
+}
