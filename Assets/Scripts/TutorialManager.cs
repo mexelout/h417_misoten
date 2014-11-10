351f7caf0e81da26549949f895857156c9f87bd7
akihisa.oyoshi <akihisa.oyoshi@gmail.com>
2014-11-10 13:02:49 +0900

    チュートリアル画面の追加(中身はテキストのみ)

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		SoundManager SoundDevice = GameObject.FindObjectOfType<SoundManager>();
+		SoundDevice.PlayBGM(0 , false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				if (Input.GetButton ("Start")) {
+					SceneManager sm = GameObject.FindObjectOfType<SceneManager> ();
+					if (sm != null) {
+						sm.NextScene ();
+					}
+			}
+	}
+}
