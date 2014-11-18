bc1494e37c2a51ac8da6f91bc2c19014e51f41fb
akihisa.oyoshi <akihisa.oyoshi@gmail.com>
2014-11-18 15:10:30 +0900

    チュートリアルシーンの更新

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class TutorialManager : MonoBehaviour {
 	public GUIText[] g;
-
 	public int i;
 	// Use this for initialization
 	void Start () {
@@ -19,13 +18,14 @@ public class TutorialManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		i++;
-		if (Input.GetButton ("Start")) {
+		 /*if (Input.GetButton ("Start")) {
 				SceneManager sm = GameObject.FindObjectOfType<SceneManager> ();
 				if (sm != null) {
 						sm.NextScene ();
 				}
-		}
-		if (i == 360) {
+		}*/
+	
+		if (false) {
 			try {
 				if (g [0].text != "tutorial" || g [1].text != "test")
 					g [0].text = "tutorial";
