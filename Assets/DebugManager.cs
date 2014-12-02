f1e7c4f95074f6782875bfeec18db9eff16cfb29
mexelout <mexelout@gmail.com>
2014-12-02 15:23:36 +0900

    デバッグマネージャー追加

---
+++ Assets/DebugManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugManager : MonoBehaviour {
+
+
+	[SerializeField]
+	private int _orderNum;
+	public int orderNum {
+		get { return _orderNum; }
+		private set { _orderNum = value; }
+	}
+
+	private int nowOrder = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(orderNum > nowOrder) {
+			nowOrder++;
+			Player p = FindObjectOfType<Player>();
+			p.transform.position = p.roadJoint.transform.position;
+			p.rigidbody.velocity = new Vector3(0, 0, 0);
+		}
+	}
+}
