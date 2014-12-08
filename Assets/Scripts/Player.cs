2dc090d87b35bd5d63cacee3ff3ae9c4a021b646
mexelout <mexelout@gmail.com>
2014-12-08 20:51:19 +0900

    プレイヤーの横移動をフローティングにした

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,13 +21,6 @@ public class Player : MonoBehaviour {
 	public float jumpPower;
 	public float jumpPowerDefault = 250;
 
-	// BoundsJoint関連
-	public Vector3 p = new Vector3(0.0f,0.0f,0.0f);
-	public Vector3 p0 =new  Vector3(0.0f,0.0f,0.0f);
-	public Vector3 p1 =new  Vector3(0.0f,0.0f,0.0f);
-	public Vector3 p2 =new  Vector3(0.0f,0.0f,0.0f);
-	public Vector3 p3 =new  Vector3(0.0f,0.0f,0.0f);
-
 	public Bezier myBezier;
 	public float t = 0f;
 
@@ -42,6 +35,8 @@ public class Player : MonoBehaviour {
 		get { return _roadJoint; }
 		private set { _roadJoint = value; }
 	}
+
+	private float offsetSide;
 
 	void Start() {
 		gm = GameObject.FindObjectOfType<GameManager>();
@@ -93,26 +88,39 @@ public class Player : MonoBehaviour {
 		}
 
 		if(roadJoint.prevJoint) {
-			Vector3 prev = roadJoint.prevJoint.transform.position + (roadJoint.prevJoint.transform.right * (roadNumber - 1) * 3);
-			prev.y = 0;
-			Vector3 next = roadJoint.transform.position + (roadJoint.transform.right * (roadNumber - 1) * 3);
-			next.y = 0;
-			Vector3 p = transform.position;
-			p.y = 0;
-			Vector3 diff = p - prev;
-			Vector3 to = next - prev;
-			float d = (to.x * diff.z - to.z * diff.x) / to.magnitude;
-			transform.position += roadJoint.prevJoint.transform.right * (d * 0.75f);
-		}
-
-		if(horizontal < 0 && !isChangeRoadNumber) {
-			roadNumber -= (roadNumber > 0) ? 1 : 0;
-			isChangeRoadNumber = true;
-		} else if(horizontal > 0 && !isChangeRoadNumber) {
-			roadNumber += (roadNumber < 2) ? 1 : 0;
-			isChangeRoadNumber = true;
-		} else if(horizontal == 0) {
-			isChangeRoadNumber = false;
+//			Vector3 prev = roadJoint.prevJoint.transform.position + (roadJoint.prevJoint.transform.right * (roadNumber - 1) * 3);
+//			prev.y = 0;
+//			Vector3 next = roadJoint.transform.position + (roadJoint.transform.right * (roadNumber - 1) * 3);
+//			next.y = 0;
+//			Vector3 p = transform.position;
+//			p.y = 0;
+//			Vector3 diff = p - prev;
+//			Vector3 to = next - prev;
+//			float d = (to.x * diff.z - to.z * diff.x) / to.magnitude;
+//			transform.position += roadJoint.prevJoint.transform.right * (d * 0.75f);
+			if(horizontal < 0) {
+				//roadNumber -= (roadNumber > 0) ? 1 : 0;
+				float movingSpeed = horizontal * 0.5f;
+				offsetSide += movingSpeed;
+				if(offsetSide < -4.5f) {
+					offsetSide = -4.5f;
+				} else {
+					transform.position += roadJoint.transform.right * movingSpeed;
+				}
+				isChangeRoadNumber = true;
+			} else if(horizontal > 0) {
+				//roadNumber += (roadNumber < 2) ? 1 : 0;
+				float movingSpeed = horizontal * 0.5f;
+				offsetSide += movingSpeed;
+				if(offsetSide > 4.5f) {
+					offsetSide = 4.5f;
+				} else {
+					transform.position += roadJoint.transform.right * movingSpeed;
+				}
+				isChangeRoadNumber = true;
+			} else if(horizontal == 0) {
+				isChangeRoadNumber = false;
+			}
 		}
 
 		if(roadJoint.name.Contains("Jump") || roadJoint.name.Contains("Parabola")) {
@@ -159,6 +167,11 @@ public class Player : MonoBehaviour {
 			RoadJoint rj = collider.gameObject.GetComponent<RoadJoint>();
 			roadJoint = rj.nextJoint.GetComponent<RoadJoint>();;
 			if(roadJoint.name.Contains("Parabola")) {
+				Vector3 p = new Vector3(0.0f,0.0f,0.0f);
+				Vector3 p0 =new  Vector3(0.0f,0.0f,0.0f);
+				Vector3 p1 =new  Vector3(0.0f,0.0f,0.0f);
+				Vector3 p2 =new  Vector3(0.0f,0.0f,0.0f);
+				Vector3 p3 =new  Vector3(0.0f,0.0f,0.0f);
 				t = 0;
 				rigidbody.velocity = new Vector3(0, 0, 0);
 				//	p0 = roadJoint.prevJoint.transform.position;
@@ -212,11 +225,11 @@ public class Player : MonoBehaviour {
 			Vector3 lookAt = t.position;
 			Vector3 copyLookAt = new Vector3(lookAt.x, lookAt.y, lookAt.z);
 			lookAt.y = transform.position.y;
-			//if(roadJoint.name.Contains("Road")) {
-				//Vector3 offset = roadJoint.transform.right * ((roadNumber - 1.0f) * 3.0f);
-				//lookAt.x += offset.x;
-				//lookAt.z += offset.z;
-			//}
+			if(roadJoint.name.Contains("Road")) {
+				Vector3 offset = roadJoint.transform.right * offsetSide;
+				lookAt.x += offset.x;
+				lookAt.z += offset.z;
+			}
 			Vector3 forward = transform.forward;
 			Vector3 targetDir = lookAt - transform.position;
 			lookAt = (targetDir - (forward * targetDir.magnitude)) * 0.1f;
